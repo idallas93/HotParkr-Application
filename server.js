01d8b094986bf449de00551f75c47307c6df286e
idallas93 <Dallas.Isaac1@gmail.com>
2020-12-07 17:07:07 -0500

    heroku test

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-
 const express = require("express");
 const mongoose = require("mongoose")
 const routes = require("./routes");
@@ -10,13 +9,14 @@ const PORT = process.env.PORT || 4000;
 app.use(express.urlencoded({ extended: true, useNewUrlParser: true  }));
 app.use(express.json());
 
+
+
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   }
-
-// app.use(passport.initialize());
-
+  
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mernPassportAuthentication");
+
 
 app.use("/", routes);
 
