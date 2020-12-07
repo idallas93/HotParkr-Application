ef5c85a8b53de6ad3d4dfc00b6c3f831aa34a425
idallas93 <Dallas.Isaac1@gmail.com>
2020-12-07 16:52:51 -0500

    fixed heroku deploy

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const PORT = process.env.PORT || 4000;
 app.use(express.urlencoded({ extended: true, useNewUrlParser: true  }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
 // app.use(passport.initialize());
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mernPassportAuthentication");
