dd9eb753ecb1071de9522ba68b53aa9d318c8a40
idallas93 <Dallas.Isaac1@gmail.com>
2020-12-07 20:00:25 -0500

    imported db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,14 @@
 const Router = require("express").Router();
+const db = require("../models");
+
 
 Router.get("/welcome", (req, res) => {
   res.json({ message: "You have gone full MERN!!!"});
+})
+
+Router.post("/api/user", async (req, res) => {
+  const user = await db.User.create(req.body)
+  res.json(user)
 })
 
 module.exports = Router;
