afc42ade9142a2d8efc650afe58bd7c18a596db7
saharVac <vcsahar@gmail.com>
2020-12-09 16:47:11 -0500

    Modified ProfileForm component and Profile page to disable fieldset until modification is desired

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,16 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import ProfileForm from "../../Components/ProfileForm"
 
 
 function Profile() {
+
+    const [updating, setUpdating] = useState(false)
+
     const updateUser = () => {
         console.log("updating")
+        // TODO: Define function updating user Info
+        setUpdating(false)
     }
 
     return (
         <main>
-            <ProfileForm isUpdating="true" updateUser={updateUser} />
+            <h1>Profile</h1>
+            <button onClick={() => setUpdating(true)}>Modify</button>
+            <ProfileForm isDisabled={!updating} isUpdating={true} updateUser={updateUser} />
         </main>
     )
 }
