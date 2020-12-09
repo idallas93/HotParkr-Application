aca7ac2bcb3178c35ea82b8117b35493e1b54587
roan1088 <rohan_188@yahoo.com>
2020-12-09 16:09:51 -0500

    Changed class to className

--- client/src/Components/ParkCard/index.js
+++ client/src/Components/ParkCard/index.js
@@ -3,11 +3,11 @@ import "./style.css";
 
 function ParkCard() {
   return (
-    <div class="card park-card">
-      <img src="https://media.gettyimages.com/photos/empty-bench-in-green-park-and-sky-with-sun-light-green-park-outdoor-picture-id1137437560" class="card-img-top" alt="..." />
-      <div class="card-body">
-        <h5 class="card-title">Park Name</h5>
-        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis suscipit ut recusandae. Fugiat officiis reiciendis incidunt, odit quibusdam qui! Accusamus sit aut quae iusto laborum quasi corporis tenetur, necessitatibus cupiditate?</p>
+    <div classNamess="card park-card">
+      <img src="https://media.gettyimages.com/photos/empty-bench-in-green-park-and-sky-with-sun-light-green-park-outdoor-picture-id1137437560" className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Park Name</h5>
+        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis suscipit ut recusandae. Fugiat officiis reiciendis incidunt, odit quibusdam qui! Accusamus sit aut quae iusto laborum quasi corporis tenetur, necessitatibus cupiditate?</p>
       </div>
     </div>
   );
