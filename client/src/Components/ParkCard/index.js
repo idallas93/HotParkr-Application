cffeb31eb23903125b9105b2f90d8310769e601c
Keith Fung <fungsenkeith@gmail.com>
2020-12-10 11:07:03 -0500

    header stuff

--- client/src/Components/ParkCard/index.js
+++ client/src/Components/ParkCard/index.js
@@ -3,11 +3,20 @@ import "./style.css";
 
 function ParkCard() {
   return (
-    <div classNamess="card park-card">
-      <img src="https://media.gettyimages.com/photos/empty-bench-in-green-park-and-sky-with-sun-light-green-park-outdoor-picture-id1137437560" className="card-img-top" alt="..." />
+    <div className="card park-card">
+      <img
+        src="https://media.gettyimages.com/photos/empty-bench-in-green-park-and-sky-with-sun-light-green-park-outdoor-picture-id1137437560"
+        className="card-img-top"
+        alt="..."
+      />
       <div className="card-body">
         <h5 className="card-title">Park Name</h5>
-        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis suscipit ut recusandae. Fugiat officiis reiciendis incidunt, odit quibusdam qui! Accusamus sit aut quae iusto laborum quasi corporis tenetur, necessitatibus cupiditate?</p>
+        <p className="card-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          suscipit ut recusandae. Fugiat officiis reiciendis incidunt, odit
+          quibusdam qui! Accusamus sit aut quae iusto laborum quasi corporis
+          tenetur, necessitatibus cupiditate?
+        </p>
       </div>
     </div>
   );
