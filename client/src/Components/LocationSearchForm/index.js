b2b06678bf081b50ea5ba4bb3860e845dbe74adc
idallas93 <Dallas.Isaac1@gmail.com>
2020-12-13 22:43:26 -0500

    moved form from home page to loginform component

--- client/src/Components/LocationSearchForm/index.js
+++ client/src/Components/LocationSearchForm/index.js
@@ -1,29 +1,50 @@
-import React from "react";
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
+import React, { useRef } from "react";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import { useGlobalContext } from "../../context/GlobalContext";
 
+function LocationSearchForm({ searchParks }) {
+  const [state, dispatch] = useGlobalContext();
+  const zipRef = useRef();
+  const radiusRef = useRef();
 
+  const searchClick = (e) => {
+    e.preventDefault()
+    const radius = radiusRef.current.value * 1609;
+    const zipcode = zipRef.current.value;
+    console.log("POOOOOOOOOOOP", radius)
+    searchParks(radius, zipcode);
+  }
 
-
-
-function LocationSearchForm() {
   return (
-    
     <Form>
-    <Row>
-      <Col>
-        <Form.Control placeholder="Search for dog park" />
-      </Col>
-      <Col>
-        <Form.Control placeholder="location" />
-      </Col>
-      <Col>
-      <Button variant="primary">Search</Button>{' '}
-      </Col>
-    </Row>
-  </Form>
+      <label htmlFor="zip">Zip-Code:</label>
+      <input
+        defaultValue={
+          state.apiToken ? JSON.parse(localStorage.getItem("user")).zipcode : ""
+        }
+        name="zip"
+        type="text"
+        ref={zipRef}
+      />
+      <label htmlFor="radius">Radius:</label>
+      <input
+        type="number"
+        name="radius"
+        min="0"
+        max="30"
+        step="0.1"
+        ref={radiusRef}
+      />
+      <Button
+        type="submit"
+        onClick={searchClick}
+      >
+        Search
+      </Button>
+    </Form>
   );
 }
 
